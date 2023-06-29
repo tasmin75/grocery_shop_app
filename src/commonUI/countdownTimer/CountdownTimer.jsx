d5caaf5a8aed1cceb1888886a93871a373f6b210
Ankush123rai <ankush98rai@gamil.com>
2023-06-30 00:00:13 +0530

    applied media query

--- src/commonUI/countdownTimer/CountdownTimer.jsx
+++ src/commonUI/countdownTimer/CountdownTimer.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import style from "./CountdownTimer.module.css";
 
 const CountdownTimer = () => {
-  return (
-    <div>CountdownTimer</div>
-  )
-}
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [sec, setSec] = useState(0);
 
-export default CountdownTimer
+  let interval;
+  const CountDown = () => {
+    const destination = new Date("July 19, 2023").getTime();
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const diff = destination - now;
+      if (diff <= 0) {
+        clearInterval(interval());
+        setDays(0);
+        setHours(0);
+        setMinutes(0);
+        setSec(0);
+      } else {
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const sec = Math.floor((diff % (1000 * 60)) / 1000);
+
+        setDays(days);
+        setHours(hours);
+        setMinutes(minutes);
+        setSec(sec);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    CountDown();
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className={style.timer_container}>
+      <div className={style.time}>
+        <div className={style.time_text}>
+          <h1>{days}</h1>
+          <h5>Days</h5>
+        </div>
+        <span>:</span>
+      </div>
+      <div className={style.time}>
+        <div className={style.time_text}>
+          <h1>{hours}</h1>
+          <h5>Hours</h5>
+        </div>
+        <span>:</span>
+      </div>
+      <div className={style.time}>
+        <div className={style.time_text}>
+          <h1>{minutes}</h1>
+          <h5>Minutes</h5>
+        </div>
+        <span>:</span>
+      </div>
+      <div className={style.time}>
+        <div className={style.time_text}>
+          <h1>{sec}</h1>
+          <h5>Seconds</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import style from './Product.module.css';
-import HeroSection2 from '../../commonUI/herosection2/HeroSection2';
+import React, { useState } from "react";
+import style from "./Product.module.css";
+import HeroSection2 from "../../commonUI/herosection2/HeroSection2";
+import {BsSearch} from 'react-icons/bs'
+import products from '../../assets/data/products'
+import Productlist from "../../components/productlist/ProductList";
 
 const Product = () => {
+
+  const [product, setProduct]=useState(products)
+
+  const handlefilterdata=(e)=>{
+    const value=e.target.value;
+    if(value==='fruits'){
+      const filteredProduct=products.filter(item => item.category==='fruits')
+      setProduct(filteredProduct)
+    }
+  }
+
   return (
     <div className={style.product_section}>
-      <HeroSection2 title="products"/>
-    </div>
-  )
-}
+      <HeroSection2 title="products" />
 
-export default Product
+      <div className={style.product_features}>
+        <div className={style.product_filter}>
+          <select onChange={handlefilterdata}>
+            <option value="all">Filtered by category</option>
+            <option value="fruits">Fruits</option>
+            <option value="vegitables">Vegitables</option>
+            <option value="rice">Rice</option>
+            <option value="flour">Flour/atta</option>
+            <option value="detergents">Detergents</option>
+            <option value="shampoo">Shampoo</option>
+          </select>
+        </div>
+        <div className={style.product_search}>
+          <input type="text" placeholder="Search..."/>
+          <div className={style.search_icon}><BsSearch/></div>
+        </div>
+      </div>
+
+      <div className={style.all_product}>
+        {
+          products.length >0 ? <Productlist data={product}/> : <h1>No product found</h1>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Product;
