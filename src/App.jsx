255506f5262487c2d471f5f226b4f4cd9d9fb7f1
Tasmin <tansarijkm25@gmail.com>
2023-07-11 22:11:59 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
       <Route  path='/login' element={<Login/>}/>
       <Route  path='/signup' element={<Signup/>}/>
     </Routes>
+    <Footer />
    </BrowserRouter>
   )
 }
